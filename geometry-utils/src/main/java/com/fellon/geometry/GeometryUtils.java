c6036e1a7f4ddea53e1d83add86415aae460321b
fellon-dnb <fellon@ancevt.com>
2025-11-26 15:09:26 +0300

    feat: в geometry-utils добавлены методы сравнения фигур, так же увеличьна версия до 1.1.0-SNAPSHOT

--- geometry-utils/src/main/java/com/fellon/geometry/GeometryUtils.java
+++ geometry-utils/src/main/java/com/fellon/geometry/GeometryUtils.java
@@ -1,11 +1,19 @@
 package com.fellon.geometry;
 
+import com.fellon.Shape;
+
 public class GeometryUtils {
 
     public static double centsToMeters(double cm) {
         return cm/100;
     }
-    public static double centsToМilimetrs(double cm){
+    public static double centsToMilimetrs(double cm){
         return cm*100;
+    }
+    public static int compareAreas(Shape shape1, Shape shape2){
+        return Double.compare(shape1.getArea(), shape2.getArea());
+    }
+    public static int comparePerimeters(Shape shape1, Shape shape2){
+        return Double.compare(shape1.getPerimeter(), shape2.getPerimeter());
     }
 }
