6554d69a1f79b727c0248cc02d85baec6da0975a
fellon-dnb <fellon@ancevt.com>
2025-11-26 14:06:36 +0300

    feat: add модуль geometry-utils и класс GeometryUtils с базовыми методами

---
+++ geometry-utils/src/main/java/com/fellon/geometry/GeometryUtils.java
@@ -0,0 +1,11 @@
+package com.fellon.geometry;
+
+public class GeometryUtils {
+
+    public static double centsToMeters(double cm) {
+        return cm/100;
+    }
+    public static double centsToМilimetrs(double cm){
+        return cm*100;
+    }
+}
